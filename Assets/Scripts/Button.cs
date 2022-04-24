99d4b2a4e5121cfdf51d335e5e411219355c50a9
jbg5721 <jbg5721@psu.edu>
2022-04-24 17:12:44 -0400

    update v2.5

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -6,12 +6,16 @@ public class Button : MonoBehaviour
 {
     public GameObject platform;
     public Animator anim;
+    public GameObject platform2;
+    public Animator anim2;
     private void OnTriggerEnter(Collider other)
     {
 
         if (other.gameObject.tag == "Player")
         {
             anim.Play("move1");
+            anim2.Play("move1");
+            
         }
 
     }
